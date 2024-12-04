98b83d2b208eccfb34ba160dec482a35aea36499
Ross Laing <30736276+RossLaing8417@users.noreply.github.com>
2024-12-04 20:59:00 +0200

    merged token into lexer

--- src/Lexer.zig
+++ src/Lexer.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 
-pub const Token = @import("token.zig");
-
 const Lexer = @This();
 
 input: []const u8,
@@ -137,6 +135,66 @@ fn peekChar(self: *Lexer) u8 {
     }
     return self.input[self.read_position];
 }
+
+pub const Token = struct {
+    type: Type,
+    literal: []const u8,
+
+    pub const Type = enum {
+        Illegal,
+        Eof,
+
+        // Literals
+        Identifier,
+        Integer,
+        String,
+
+        // Operators
+        Assign,
+        Plus,
+        Minus,
+        Asterisk,
+        Slash,
+        Bang,
+        LessThan,
+        GreaterThan,
+        Equal,
+        NotEqual,
+
+        // Symbols
+        Comma,
+        SemiColon,
+        LeftParen,
+        RightParen,
+        LeftBrace,
+        RightBrace,
+        LeftBracket,
+        RightBracket,
+
+        // Keywords
+        Function,
+        Let,
+        True,
+        False,
+        If,
+        Else,
+        Return,
+    };
+
+    const map = std.StaticStringMap(Type).initComptime(.{
+        .{ "fn", .Function },
+        .{ "let", .Let },
+        .{ "true", .True },
+        .{ "false", .False },
+        .{ "if", .If },
+        .{ "else", .Else },
+        .{ "return", .Return },
+    });
+
+    pub fn lookupIdentifier(literal: []const u8) ?Type {
+        return map.get(literal);
+    }
+};
 
 test "Lexer" {
     const input =
